f3402d83d8e4d8fcf82acda857d62dc8887bf688
wangyaoyu <2773354167@qq.com>
2020-02-25 16:39:49 +0800

    add administartion

---
+++ jcart-administartion-back/src/main/java/com/bw/jcartadministartionback/JcartAdministartionBackApplication.java
@@ -0,0 +1,13 @@
+package com.bw.jcartadministartionback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartAdministartionBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartAdministartionBackApplication.class, args);
+    }
+
+}
---
+++ jcart-administartion-back/src/test/java/com/bw/jcartadministartionback/JcartAdministartionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bw.jcartadministartionback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartAdministartionBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
